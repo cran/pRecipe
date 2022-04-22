067d74370ea248ecc2c8b79cf0c7c2b54b8d525a
Mijael Rodrigo Vargas Godoy <mirovago@gmail.com>
2022-04-22 07:40:02 +0000

    version 0.2.0

---
+++ inst/doc/pRecipe.R
@@ -0,0 +1,20 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  echo = TRUE,
+  eval = FALSE
+)
+
+## -----------------------------------------------------------------------------
+#  library(pRecipe)
+#  download_data() #(default) download and reformat all the data sets listed above.
+
+## -----------------------------------------------------------------------------
+#  precip <- import_subset_data(name = "all", start_year = 1836, end_year = 2020,
+#                               bbox = c(12.25, 48.75, 18.75, 50.75))
+
+## -----------------------------------------------------------------------------
+#  precip <- crop_data(precip, "cz_1km.shp")
+
+## -----------------------------------------------------------------------------
+#  plot_box(precip[year(Z)>2000 & year(Z)<=2020])
+
