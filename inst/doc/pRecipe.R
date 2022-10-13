47663f77e371057dcdf6ab9d315d8b73a9a6f12f
Mijael Rodrigo Vargas Godoy <mirovago@gmail.com>
2022-10-13 10:20:02 +0000

    version 0.4.0

--- inst/doc/pRecipe.R
+++ inst/doc/pRecipe.R
@@ -1,80 +1,85 @@
 ## ---- include = FALSE---------------------------------------------------------
 knitr::opts_chunk$set(
   echo = TRUE,
-  eval = FALSE
+  eval = TRUE,
+  fig.width = 7,
+  warning = FALSE,
+  message = FALSE
 )
 
-## -----------------------------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  install.packages('pRecipe')
 #  library(pRecipe)
 
-## -----------------------------------------------------------------------------
-#  download_data(name = "era5", project_folder = ".")
-#  show_info("./data/database/era5_tp_mm_global_195901_202112_025_monthly.nc")
+## ---- eval=FALSE--------------------------------------------------------------
+#  download_data(name = "era5", destination = ".")
+#  show_info("era5_tp_mm_global_195901_202112_025_monthly.nc")
 
-## -----------------------------------------------------------------------------
-#  subset_spacetime("era5", start_year = 1981, end_year = 2020,
-#                   bbox = c(2,28,42,58),
-#                   database_path = "./data/database/")
-#  show_info("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly.nc")
+## ---- eval=FALSE--------------------------------------------------------------
+#  subset_spacetime("era5_tp_mm_global_195901_202112_025_monthly.nc",
+#                   years = c(1981, 2020), bbox = c(2,28,42,58))
+#  show_info("era5_tp_mm_subset_198101_202012_025_monthly.nc")
 
-## -----------------------------------------------------------------------------
-#  crop_data(nc_path = "./data/processed/era5_tp_mm_subset_1981_2020_025_monthly.nc",
-#            shp_path = "./shapefiles/CZE_adm0.shp",
-#            save_nc = TRUE)
-#  show_info("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly_cropped.nc")
+## ---- eval=FALSE--------------------------------------------------------------
+#  crop_data(data_file = "era5_tp_mm_subset_198101_202012_025_monthly.nc",
+#            shp_path = "CZE_adm0.shp")
+#  show_info("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
 
-## -----------------------------------------------------------------------------
-#  make_ts("era5", database_path = "./data/database/")
-#  tp_ts <- readr::read_csv("./data/processed/era5_ts.csv")
-#  head(tp_ts, 12)
+## ---- eval=FALSE--------------------------------------------------------------
+#  make_ts("era5_tp_mm_global_195901_202112_025_monthly.nc")
+#  era5_global_ts <- fread("era5_tp_mm_global_195901_202112_025_monthly_ts.csv")
+#  head(era5_global_ts, 12)
 
-## -----------------------------------------------------------------------------
-#  era5_subset <- raster::brick("./data/processed/era5_tp.mm_subset_1981_2020_025_monthly.nc")
-#  make_ts(era5_subset)
-#  subset_ts <- readr::read_csv("./data/processed/era5_tp.mm_subset_1981_2020_025_monthly_ts.csv")
-#  head(subset_ts, 12)
+## ---- eval=FALSE--------------------------------------------------------------
+#  make_ts("era5_tp_mm_subset_198101_202012_025_monthly.nc")
+#  era5_ce_ts <- fread("era5_tp_mm_subset_198101_202012_025_monthly_ts.csv")
+#  head(era5_ce_ts, 12)
 
-## -----------------------------------------------------------------------------
-#  era5_cropped <- raster::brick("./data/processed/era5_tp.mm_subset_1981_2020_025_monthly_cropped.nc")
-#  make_ts(era5_cropped)
-#  cropped_ts <- readr::read_csv("./data/processed/era5_tp.mm_subset_1981_2020_025_monthly_cropped_ts.csv")
-#  head(cropped_ts, 12)
+## ---- eval=FALSE--------------------------------------------------------------
+#  make_ts("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
+#  era5_cze_ts <- fread("era5_tp_mm_cropped_198101_202012_025_monthly_ts.csv")
+#  head(era5_cze_ts, 12)
 
-## -----------------------------------------------------------------------------
-#  global <- raster::raster("./data/database/era5_tp_mm_global_195901_202112_025_monthly.nc")
+## ---- eval=FALSE--------------------------------------------------------------
+#  global <- brick("era5_tp_mm_global_195901_202112_025_monthly.nc")
 #  plot_map(global[[1]])
 
-## -----------------------------------------------------------------------------
-#  central_europe <- raster::brick("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly.nc")
+## ---- eval=FALSE--------------------------------------------------------------
+#  central_europe <- brick("era5_tp_mm_subset_198101_202012_025_monthly.nc")
 #  plot_map(central_europe[[1]])
 
-## -----------------------------------------------------------------------------
-#  czechia <- raster::brick("./data/processed/era5_tp_mm_subset_1981_2020_025_monthly_cropped.nc")
+## ---- eval=FALSE--------------------------------------------------------------
+#  czechia <- brick("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
 #  plot_map(czechia[[1]])
 
-## -----------------------------------------------------------------------------
-#  plot_line(tp_ts)
-#  #plot_line(subset_ts)
-#  #plot_line(cropped_ts)
+## ---- include=FALSE-----------------------------------------------------------
+library(pRecipe)
+data("era5_global_ts")
+data("era5_ce_ts")
+data("era5_cze_ts")
 
 ## -----------------------------------------------------------------------------
-#  plot_heatmap(tp_ts)
-#  #plot_heatmap(subset_ts)
-#  #plot_heatmap(cropped_ts)
+plot_line(era5_global_ts)
+plot_line(era5_ce_ts)
+plot_line(era5_cze_ts)
 
 ## -----------------------------------------------------------------------------
-#  plot_box(tp_ts)
-#  #plot_box(subset_ts)
-#  #plot_box(cropped_ts)
+plot_heatmap(era5_global_ts)
+plot_heatmap(era5_ce_ts)
+plot_heatmap(era5_cze_ts)
 
 ## -----------------------------------------------------------------------------
-#  plot_density(tp_ts)
-#  #plot_density(subset_ts)
-#  #plot_density(cropped_ts)
+plot_box(era5_global_ts)
+plot_box(era5_ce_ts)
+plot_box(era5_cze_ts)
 
 ## -----------------------------------------------------------------------------
-#  plot_summary(tp_ts)
-#  #plot_summary(subset_ts)
-#  #plot_summary(cropped_ts)
+plot_density(era5_global_ts)
+plot_density(era5_ce_ts)
+plot_density(era5_cze_ts)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  plot_summary(era5_global_ts)
+#  #plot_summary(era5_ce_ts)
+#  #plot_summary(era5_cze_ts)
 
