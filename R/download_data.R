23eeecea6cbbbddb7db0c965fbaad1478dd2d981
Mijael Rodrigo Vargas Godoy <mirovago@gmail.com>
2023-04-08 21:30:07 +0000

    version 2.3.0

--- R/download_data.R
+++ R/download_data.R
@@ -14,6 +14,7 @@
 #' \item{"em-earth" for EM-EARTH,}
 #' \item{"era20c" for ERA-20C,}
 #' \item{"era5" for ERA5,}
+#' \item{"fldas" for FLDAS,}
 #' \item{"ghcn" for GHCN-M v2,}
 #' \item{"gldas-clsm" for GLDAS CLSM,}
 #' \item{"gldas-noah" for GLDAS NOAH,}
@@ -21,6 +22,8 @@
 #' \item{"gpcc" for GPCC v2020,}
 #' \item{"gpcp" for GPCP v2.3,}
 #' \item{"gpm_imerg" for GPM IMERGM Final v06,}
+#' \item{"jra55" for JRA-55,}
+#' \item{"merra2" for MERRA-2,}
 #' \item{"mswep" for MSWEP v2.8,}
 #' \item{"ncep-doe" for NCEP/DOE,}
 #' \item{"ncep-ncar" for NCEP/NCAR,}
@@ -30,45 +33,60 @@
 #' \item{"trmm-3b43" for TRMM 3B43 v7,}
 #' \item{"udel" for UDEL v501.}
 #' }
-#' @param destination a character string with the path where the database will be downloaded.
+#' @param path a character string with the path where the database will be downloaded.
+#' @param domain a character string with the desired domain data set. Suitable options are:
+#' \itemize{
+#' \item{"raw" for default available spatial coverage,}
+#' \item{"global" for data sets with global (land and ocean) coverage,}
+#' \item{"land" for data sets with land only coverage,}
+#' \item{"ocean", for data sets with ocean only coverage.}
+#' }
+#' @param time_res a character string with the desired time resolution. Suitable options are:
+#' \itemize{
+#' \item{"monthly",}
+#' \item{"yearly".}
+#' }
 #' @return No return value, called to download the required data sets.
 #' @export
 #' @examples
 #' \donttest{
-#' download_data("gldas-vic", tempdir())
+#' download_data("gldas-vic", tempdir(), time_res = "yearly")
 #' }
 
-download_data <- function(data_name = "all", destination = "."){
+download_data <- function(data_name = "all", path = ".", domain = "raw", time_res = "monthly"){
   dataset_check(data_name)
   old_options <- options()
   options(timeout = 6000)
   on.exit(options(old_options))
   lapply(data_name, function(dataset) switch(dataset,
-                                        "all"  = download_all(destination),
-                                        "20cr" = download_20cr(destination),
-                                        "chirps" = download_chirps(destination),
-                                        "cmap" = download_cmap(destination),
-                                        "cmorph" = download_cmorph(destination),
-                                        "cpc" = download_cpc(destination),
-                                        "cru-ts" = download_cru_ts(destination),
-                                        "em-earth" = download_em_earth(destination),
-                                        "era20c" = download_era20c(destination),
-                                        "era5" = download_era5(destination),
-                                        "ghcn" = download_ghcn(destination),
-                                        "gldas-clsm" = download_gldas_clsm(destination),
-                                        "gldas-noah" = download_gldas_noah(destination),
-                                        "gldas-vic" = download_gldas_vic(destination),
-                                        "gpcc" = download_gpcc(destination),
-                                        "gpcp" = download_gpcp(destination),
-                                        "gpm-imerg" = download_gpm_imerg(destination),
-                                        "mswep" = download_mswep(destination),
-                                        "ncep-doe" = download_ncep_doe(destination),
-                                        "ncep-ncar" = download_ncep_ncar(destination),
-                                        "persiann" = download_persiann(destination),
-                                        "precl" = download_precl(destination),
-                                        "terraclimate" = download_terraclimate(destination),
-                                        "trmm-3b43" = download_trmm_3b43(destination),
-                                        "udel" = download_udel(destination)
+                                        "all"  = download_all(path, domain, time_res),
+                                        "20cr" = download_20cr(path, domain, time_res),
+                                        "chirps" = download_chirps(path, domain, time_res),
+                                        "cmap" = download_cmap(path, domain, time_res),
+                                        "cmorph" = download_cmorph(path, domain, time_res),
+                                        "cpc" = download_cpc(path, domain, time_res),
+                                        "cru-ts" = download_cru_ts(path, domain, time_res),
+                                        "em-earth" = download_em_earth(path, domain, time_res),
+                                        "era20c" = download_era20c(path, domain, time_res),
+                                        "era5" = download_era5(path, domain, time_res),
+                                        "fldas" = download_fldas(path, domain, time_res),
+                                        "ghcn" = download_ghcn(path, domain, time_res),
+                                        "gldas-clsm" = download_gldas_clsm(path, domain, time_res),
+                                        "gldas-noah" = download_gldas_noah(path, domain, time_res),
+                                        "gldas-vic" = download_gldas_vic(path, domain, time_res),
+                                        "gpcc" = download_gpcc(path, domain, time_res),
+                                        "gpcp" = download_gpcp(path, domain, time_res),
+                                        "gpm-imerg" = download_gpm_imerg(path, domain, time_res),
+                                        "jra55" = download_jra55(path, domain, time_res),
+                                        "merra2" = download_merra2(path, domain, time_res),
+                                        "mswep" = download_mswep(path, domain, time_res),
+                                        "ncep-doe" = download_ncep_doe(path, domain, time_res),
+                                        "ncep-ncar" = download_ncep_ncar(path, domain, time_res),
+                                        "persiann" = download_persiann(path, domain, time_res),
+                                        "precl" = download_precl(path, domain, time_res),
+                                        "terraclimate" = download_terraclimate(path, domain, time_res),
+                                        "trmm-3b43" = download_trmm_3b43(path, domain, time_res),
+                                        "udel" = download_udel(path, domain, time_res)
   ))
   return(invisible())
 }
--- inst/doc/pRecipe.R
+++ inst/doc/pRecipe.R
@@ -6,60 +6,52 @@ knitr::opts_chunk$set(
   warning = FALSE,
   message = FALSE
 )
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  install.packages('pRecipe')
-#  library(pRecipe)
-#  #Load additional packages
-#  library(data.table)
-#  library(raster)
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  download_data(data_name = "era5", destination = ".")
-#  show_info("era5_tp_mm_global_195901_202112_025_monthly.nc")
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  subset_spacetime("era5_tp_mm_global_195901_202112_025_monthly.nc",
-#                   years = c(1981, 2020), bbox = c(2,28,42,58))
-#  show_info("era5_tp_mm_subset_198101_202012_025_monthly.nc")
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  crop_data(data_file = "era5_tp_mm_subset_198101_202012_025_monthly.nc",
-#            shp_path = "CZE_adm0.shp")
-#  show_info("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  make_ts("era5_tp_mm_global_195901_202112_025_monthly.nc")
-#  era5_global_ts <- fread("era5_tp_mm_global_195901_202112_025_monthly_ts.csv")
-#  head(era5_global_ts, 12)
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  make_ts("era5_tp_mm_subset_198101_202012_025_monthly.nc")
-#  era5_ce_ts <- fread("era5_tp_mm_subset_198101_202012_025_monthly_ts.csv")
-#  head(era5_ce_ts, 12)
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  make_ts("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
-#  era5_cze_ts <- fread("era5_tp_mm_cropped_198101_202012_025_monthly_ts.csv")
-#  head(era5_cze_ts, 12)
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  global <- brick("era5_tp_mm_global_195901_202112_025_monthly.nc")
-#  plot_map(global[[1]])
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  central_europe <- brick("era5_tp_mm_subset_198101_202012_025_monthly.nc")
-#  plot_map(central_europe[[1]])
-
-## ---- eval=FALSE--------------------------------------------------------------
-#  czechia <- brick("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
-#  plot_map(czechia[[1]])
-
-## ---- include=FALSE-----------------------------------------------------------
 library(pRecipe)
 data("era5_global_ts")
 data("era5_ce_ts")
 data("era5_cze_ts")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  install.packages('pRecipe')
+#  library(pRecipe)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  download_data(data_name = "era5")
+#  show_info("era5_tp_mm_global_195901_202112_025_monthly.nc")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  subset_spacetime("era5_tp_mm_global_195901_202112_025_monthly.nc",
+#                   years = c(1981, 2020), bbox = c(2,28,42,58), autosave = TRUE)
+#  show_info("era5_tp_mm_subset_198101_202012_025_monthly.nc")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  crop_data(data_file = "era5_tp_mm_subset_198101_202012_025_monthly.nc",
+#            shp_path = "CZE_adm0.shp", autosave = TRUE)
+#  show_info("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  era5_global_ts <- make_ts("era5_tp_mm_global_195901_202112_025_monthly.nc")
+#  head(era5_global_ts, 12)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  era5_ce_ts <- make_ts("era5_tp_mm_subset_198101_202012_025_monthly.nc")
+#  head(era5_ce_ts, 12)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  era5_cze_ts <-make_ts("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
+#  head(era5_cze_ts, 12)
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  global <- raster::brick("era5_tp_mm_global_195901_202112_025_monthly.nc")
+#  plot_map(global[[1]])
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  central_europe <- raster::brick("era5_tp_mm_subset_198101_202012_025_monthly.nc")
+#  plot_map(central_europe[[1]])
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  czechia <- raster::brick("era5_tp_mm_cropped_198101_202012_025_monthly.nc")
+#  plot_map(czechia[[1]])
 
 ## -----------------------------------------------------------------------------
 plot_line(era5_global_ts)
