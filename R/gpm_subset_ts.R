f59ac986675baa7e66ae5a5ba09578ff56068065
Mijael Rodrigo Vargas Godoy <mirovago@gmail.com>
2023-07-10 19:10:02 +0000

    version 3.0.0

--- R/gpm_subset_ts.R
+++ R/gpm_subset_ts.R
@@ -1,13 +1,11 @@
 #' Monthly precipitation data
 #'
-#' A subset of GPM-IMERGM monthly precipitation data in mm over 2-28E, 42-58N. More details of the raw data can be found \href{https://gpm.nasa.gov/data/imerg}{here}.
+#' A subset of GPM-IMERGM monthly precipitation data in mm over South America. More details of the raw data can be found \href{https://gpm.nasa.gov/data/imerg}{here}.
 #'
-#' @format A data.table with 120 obs. of 4 variables:
+#' @format A data.table with 180 obs. of 2 variables:
 #' \describe{
 #'   \item{date}{Date format \%Y-\%m-\%d}
 #'   \item{value}{monthly average values}
-#'   \item{name}{full name of the data set}
-#'   \item{type}{source type of the data set}
 #' }
 #' @source National Aeronautics and Space Administration (NASA)
 "gpm_subset_ts"
